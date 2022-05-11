2f4e96ba7096b0004d8752428ffc68936bcec435
setname <setemail>
2022-05-11 16:24:55 +0100

    Define Query type used as entry points to graph data

--- catstronauts-server/src/schema.js
+++ catstronauts-server/src/schema.js
@@ -3,6 +3,12 @@ const {gql} = require('apollo-server');
 const typeDefs = gql`
 #Schema definitions go here
 
+"Query type's filds are entry points to graph's data"
+type Query{
+    "Get tracks array for homepage grid"
+    tracksForHome:[Track!]!
+}
+
 "A track is a group of Modules that teaches about a specific topic"
 type Track{
     id: ID!
