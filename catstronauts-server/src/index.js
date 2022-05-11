9e74ad7fa18bb9e9a17328cc0a79fa787368765a
setname <setemail>
2022-05-11 16:55:02 +0100

    Start listening for server and add console message

--- catstronauts-server/src/index.js
+++ catstronauts-server/src/index.js
@@ -3,3 +3,11 @@ const {AolloServer} = require('apollo-server');
 const typeDefs = require('./schema');
 
 const server = AolloServer({typeDefs});
+
+server.listen().then(()=>{
+    console.log(`
+    🚀  Server is running!
+    🔉  Listening on port 4000
+    📭  Query at https://studio.apollographql.com/dev
+  `);
+});
