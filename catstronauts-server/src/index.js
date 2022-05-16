5ccfa050933b346335278deca75584f069266b08
setname <setemail>
2022-05-16 17:12:20 +0100

    Fix moduleCount field name in mocks

--- catstronauts-server/src/index.js
+++ catstronauts-server/src/index.js
@@ -20,7 +20,7 @@ const mocks = {
       thumbnail: () =>
         'https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg',
       length: () => 1210,
-      modulesCount: () => 6
+      moduleCount: () => 6
     })
   };
 
